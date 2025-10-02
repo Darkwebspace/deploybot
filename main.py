c1b676110ac45a70364159127b9b4d1db6e273e6
Darkwebspace <patrosridhar96@gmail.com>
2025-10-02 09:24:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import logging
-from dotenv import load_dotenv 
-load_dotenv()
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import json
